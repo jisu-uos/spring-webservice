0d29bbbe09e1c83f649273343eb1d07e82e5eb13
jisu-uos <chlwltn6330@naver.com>
2020-07-28 18:12:10 +0900

    Init

---
+++ src/test/java/com/gazua/webservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gazua.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
